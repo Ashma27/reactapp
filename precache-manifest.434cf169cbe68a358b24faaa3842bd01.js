59ca489a2246053374823f3bb771f842a7f0ac3f
ashma27 <ashma.designoweb@gmail.com>
2020-09-28 17:43:01 +0530

    Updates

---
+++ precache-manifest.434cf169cbe68a358b24faaa3842bd01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913d3b8ff423b43b56c30d29606bf057",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "40649a1f06733dc1f57f",
+    "url": "/reactapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f409b96a11e8e41e34cf",
+    "url": "/reactapp/static/css/main.087f8a91.chunk.css"
+  },
+  {
+    "revision": "40649a1f06733dc1f57f",
+    "url": "/reactapp/static/js/2.80485c02.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/reactapp/static/js/2.80485c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f409b96a11e8e41e34cf",
+    "url": "/reactapp/static/js/main.d83d3404.chunk.js"
+  },
+  {
+    "revision": "8b74a0196e63e3c7023b",
+    "url": "/reactapp/static/js/runtime-main.b13f3f29.js"
+  }
+]);
